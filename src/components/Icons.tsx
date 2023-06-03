147ecc5d4f98ba19e9eee53b0975b00fefdcbe47
Jean Pierre <jeanpierre.webdev@gmail.com>
2023-06-03 00:47:33 -0500

    feat: add navbar icons

---
+++ src/components/Icons.tsx
@@ -0,0 +1,17 @@
+export function SearchIcon () {
+  return (
+    <svg xmlns='http://www.w3.org/2000/svg' width={12} height={12} fill='none' stroke='currentColor' strokeWidth={5.3} aria-hidden='true' viewBox='0 0 32 32'><path d='M13 24a11 11 0 1 0 0-22 11 11 0 0 0 0 22zm8-3 9 9' /></svg>
+  )
+}
+
+export function OutlineMenuIcon () {
+  return (
+    <svg xmlns='http://www.w3.org/2000/svg' aria-hidden='true' fill='none' height={16} width={16} stroke='currentColor' strokeWidth={3} viewBox='0 0 32 32'><path d='M2 16h28M2 24h28M2 8h28' /></svg>
+  )
+}
+
+export function AvatarIcon () {
+  return (
+    <svg xmlns='http://www.w3.org/2000/svg' aria-hidden='true' width={30} height={30} fill='currentColor' viewBox='0 0 32 32'><path d='M16 .7C7.56.7.7 7.56.7 16S7.56 31.3 16 31.3 31.3 24.44 31.3 16 24.44.7 16 .7zm0 28c-4.02 0-7.6-1.88-9.93-4.81a12.43 12.43 0 0 1 6.45-4.4A6.5 6.5 0 0 1 9.5 14a6.5 6.5 0 0 1 13 0 6.51 6.51 0 0 1-3.02 5.5 12.42 12.42 0 0 1 6.45 4.4A12.67 12.67 0 0 1 16 28.7z' /></svg>
+  )
+}
